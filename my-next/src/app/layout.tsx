6c048fafc60e84755605578a5227b020e03f12fc
AikoWada314 <aiko.lifestyle314@gmail.com>
2025-09-19 14:24:51 +0900

    chapter8

--- my-next/src/app/layout.tsx
+++ my-next/src/app/layout.tsx
@@ -1,6 +1,9 @@
+
+
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "./_components/Header/Header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,10 +27,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
         {children}
       </body>
     </html>
